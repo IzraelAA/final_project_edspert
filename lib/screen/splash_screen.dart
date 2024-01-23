c5e1f52c203e52601e487f6437f0eaf46cd1a940
IzraelAA <acari.panca21@gmail.com>
2024-01-23 19:49:18 +0700

    Initial commit

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'package:final_project_edspert/screen/login_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text('NONTON·ID')],
+      ),
+    );
+  }
+}
